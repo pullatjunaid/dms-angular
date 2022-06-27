2a8ee6b603be6c561ca01d89a844522f42d57f3d
junaid pullat <junaid.pullat@mobillionlabs.com>
2022-06-27 16:46:37 +0530

    build fix

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,4 @@
 export const environment = {
   production: true,
-  // apiBaseUrl: 'http://10.10.37.72:8000/api',
-  apiBaseUrl: 'http://localhost/dms-laravel/api',
+  apiBaseUrl: 'http://10.10.37.72:8000/api',
 };
