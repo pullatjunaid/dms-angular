d328f4dcbbfabea3bfd5a6636262accac845a76d
junaid pullat <junaid.pullat@mobillionlabs.com>
2022-06-27 15:52:53 +0530

    jun27

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { AuthService } from 'src/app/core/services/auth/auth.service';
+import { customTosters } from 'src/app/core/utils/toaster';
 
 @Component({
   selector: 'app-login',
@@ -13,7 +14,11 @@ export class LoginComponent implements OnInit {
   loginErrorMessage: string = '';
   isLoadingLogin: boolean = false;
 
-  constructor(private authService: AuthService, private router: Router) {}
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+    private customToster: customTosters
+  ) {}
 
   ngOnInit(): void {
     // this.authService.doSignup().subscribe((res) => {});
@@ -41,8 +46,9 @@ export class LoginComponent implements OnInit {
           localStorage.setItem('api_token', res?.api_token);
           localStorage.setItem('userDetails', JSON.stringify(res?.user));
           localStorage.setItem('permissions', JSON.stringify(res?.permissions));
+          this.customToster.simpleToaster('Welcome to DMS');
           this.router.navigate(['/dashboard']);
-          // window.location.href = '/dms';
+          window.location.href = '/';
         },
         (err: any) => {
           this.isLoadingLogin = false;
